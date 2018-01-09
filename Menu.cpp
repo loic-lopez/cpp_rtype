9dde65f1f0ea40c868ac0fede78a6b34cd38389a
Stan <stanislas.deneubourg@epitech.eu>
2018-01-09 14:20:37 +0100

    menu class

---
+++ Menu.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Zakelis on 09/01/2018.
+//
+
+#include "Menu.h"
+
+Menu::Menu() {
+
+}
+
+Menu::~Menu() {
+
+}
+
+void Menu::initMenu(const std::string &path) {
+
+}
+
+void Menu::drawMenu(sf::RenderWindow &App) {
+
+}
