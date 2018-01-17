13b398cd7a8f8b2ad5cc5fe96097c0ddad0dcd70
loiclopez <loic.lopez@epitech.eu>
2018-01-17 15:38:07 +0100

    add lvl4 bg

--- src/Level/ALevel.cpp
+++ src/Level/ALevel.cpp
@@ -5,7 +5,8 @@
 #include <Level/ALevel.h>
 #include <Handler/GameHandler.h>
 
-ALevel::ALevel() : player(GameHandler::Instance().getPlayer()), hud(GameHandler::Instance().getHud()) {
+ALevel::ALevel() : player(GameHandler::Instance().getPlayer()), hud(GameHandler::Instance().getHud())
+{
     this->isGameLost = false;
     this->transitionToGameOverScreenTexture.loadFromFile("./ressources/gameOverScreen/game_over_background.jpg");
     this->transitionToGameOverScreenSprite.setTexture(this->transitionToGameOverScreenTexture);
@@ -25,8 +26,10 @@ ALevel::~ALevel()
 {
 }
 
-void ALevel::controller() {
-    if (keyboard.isKeyPressed(sf::Keyboard::LShift)) {
+void ALevel::controller()
+{
+    if (keyboard.isKeyPressed(sf::Keyboard::LShift))
+    {
         if (keyboard.isKeyPressed(sf::Keyboard::Z))
             player->move(sf::Vector2f(0, -2));
         if (keyboard.isKeyPressed(sf::Keyboard::S))
@@ -35,7 +38,9 @@ void ALevel::controller() {
             player->move(sf::Vector2f(-2, 0));
         if (keyboard.isKeyPressed(sf::Keyboard::D))
             player->move(sf::Vector2f(2, 0));
-    } else {
+    }
+    else
+    {
         if (keyboard.isKeyPressed(sf::Keyboard::Z))
             player->move(sf::Vector2f(0, -1));
         if (keyboard.isKeyPressed(sf::Keyboard::S))
@@ -48,12 +53,14 @@ void ALevel::controller() {
 
     if (keyboard.isKeyPressed(sf::Keyboard::Space))
         player->shoot();
-    if (keyboard.isKeyPressed(sf::Keyboard::Escape)) {
+    if (keyboard.isKeyPressed(sf::Keyboard::Escape))
+    {
         WindowProperties::gameState = GameState::CLOSE;
     }
 }
 
-void ALevel::XboxController() {
+void ALevel::XboxController()
+{
     float posX;
     float posY;
     sf::Vector2f speed;
@@ -63,42 +70,52 @@ void ALevel::XboxController() {
     posY = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
     speed = sf::Vector2f((posX / 100), (posY / 100));
     player->move(speed);
-    if (sf::Joystick::isButtonPressed(0, 0)) {
+    if (sf::Joystick::isButtonPressed(0, 0))
+    {
         player->shoot();
     }
-    if (sf::Joystick::isButtonPressed(0, 1)) {
+    if (sf::Joystick::isButtonPressed(0, 1))
+    {
         std::cout << "B" << std::endl;
     }
-    if (sf::Joystick::isButtonPressed(0, 2)) {
+    if (sf::Joystick::isButtonPressed(0, 2))
+    {
         std::cout << "zqdqzd" << std::endl;
     }
-    if (sf::Joystick::isButtonPressed(0, 3)) {
+    if (sf::Joystick::isButtonPressed(0, 3))
+    {
         std::cout << "Y" << std::endl;
 
     }
-    if (sf::Joystick::isButtonPressed(0, 4)) {
+    if (sf::Joystick::isButtonPressed(0, 4))
+    {
         std::cout << "l1" << std::endl;
 
     }
-    if (sf::Joystick::isButtonPressed(0, 5)) {
+    if (sf::Joystick::isButtonPressed(0, 5))
+    {
         std::cout << "R2" << std::endl;
 
     }
-    if (sf::Joystick::isButtonPressed(0, 6)) {
+    if (sf::Joystick::isButtonPressed(0, 6))
+    {
         std::cout << "Back" << std::endl;
 
     }
-    if (sf::Joystick::isButtonPressed(0, 7)) {
+    if (sf::Joystick::isButtonPressed(0, 7))
+    {
         std::cout << "start" << std::endl;
 
     }
-    if (sf::Joystick::isButtonPressed(0, 8)) {
+    if (sf::Joystick::isButtonPressed(0, 8))
+    {
         std::cout << "L3" << std::endl;
 
     }
 }
 
-void ALevel::drawAll(sf::RenderWindow &App) {
+void ALevel::drawAll(sf::RenderWindow &App)
+{
     this->drawLvl(App);
     for (auto &bullet : bulletsEnemy)
         bullet->drawSprite(App);
@@ -112,45 +129,56 @@ void ALevel::drawAll(sf::RenderWindow &App) {
         App.draw(this->transitionToGameOverScreenSprite);
 }
 
-void ALevel::updateAlliedBullet() {
-    for (size_t i = 0; i < this->bulletsAllied.size(); i++) {
-        if (!bulletsAllied[i]->outOfBounds()) {
+void ALevel::updateAlliedBullet()
+{
+    for (size_t i = 0; i < this->bulletsAllied.size(); i++)
+    {
+        if (!bulletsAllied[i]->outOfBounds())
+        {
             this->bulletsAllied.erase(this->bulletsAllied.begin() + i);
             i--;
-        } else
+        }
+        else
             this->bulletsAllied[i]->updatePos();
     }
-    for (size_t i = 0; i < this->bulletsEnemy.size(); i++) {
-        if (!bulletsEnemy[i]->outOfBounds()) {
+    for (size_t i = 0; i < this->bulletsEnemy.size(); i++)
+    {
+        if (!bulletsEnemy[i]->outOfBounds())
+        {
             this->bulletsEnemy.erase(this->bulletsEnemy.begin() + i);
             i--;
-        } else
+        }
+        else
             this->bulletsEnemy[i]->updatePos();
     }
 }
 
-void ALevel::updateEntities() {
+void ALevel::updateEntities()
+{
     player->updatePos();
-    for (size_t i = 0; i < ennemies.size(); i++) {
+    for (size_t i = 0; i < ennemies.size(); i++)
+    {
         this->ennemies[i]->updatePos();
         this->ennemies[i]->shoot();
     }
 }
 
-
-void ALevel::addBullet(IEntity *newBullet) {
+void ALevel::addBullet(IEntity *newBullet)
+{
     if (((Bullet *) newBullet)->getSide() == Side::ENEMY)
         bulletsEnemy.push_back(std::shared_ptr<IEntity>(newBullet));
     else
         bulletsAllied.push_back(std::shared_ptr<IEntity>(newBullet));
 }
 
-void ALevel::checkEntitiesBoxes() {
+void ALevel::checkEntitiesBoxes()
+{
     for (auto it = bulletsAllied.begin(); it < bulletsAllied.end(); ++it)
         if (!ennemies.empty())
-            for (auto enemy = ennemies.begin(); enemy < ennemies.end(); ++enemy) {
-                if ((*it)->getHitBox().intersects((*enemy)->getHitBox())) {
-
+            for (auto enemy = ennemies.begin(); enemy < ennemies.end(); ++enemy)
+            {
+                if ((*it)->getHitBox().intersects((*enemy)->getHitBox()))
+                {
                     if ((*enemy)->getType() == Textures::ENEMY1)
                         ennemies.erase(enemy);
                     else if ((*enemy)->getType() == Textures::ENEMY2)
@@ -158,16 +186,19 @@ void ALevel::checkEntitiesBoxes() {
                         if ((*enemy)->getHp() <= 0)
                             ennemies.erase(enemy);
                         else
-                        (*enemy)->setHp((*enemy)->getHp() - 1);
+                            (*enemy)->setHp((*enemy)->getHp() - 1);
                     }
                     bulletsAllied.erase(it);
                     break;
                 }
             }
 
-    for (auto it = bulletsEnemy.begin(); it != bulletsEnemy.end(); ++it) {
-        if ((*it)->getHitBox().intersects(player->getHitBox())) {
-            if (this->player->getHp() > 0 && invulnerabilityTime.asMilliseconds() > 1500) {
+    for (auto it = bulletsEnemy.begin(); it != bulletsEnemy.end(); ++it)
+    {
+        if ((*it)->getHitBox().intersects(player->getHitBox()))
+        {
+            if (this->player->getHp() > 0 && invulnerabilityTime.asMilliseconds() > 1500)
+            {
                 inv.restart();
                 this->player->setHp(this->player->getHp() - 1);
                 bulletsEnemy.erase(it);
@@ -177,30 +208,40 @@ void ALevel::checkEntitiesBoxes() {
     }
 }
 
-void ALevel::initLvl(const std::string &path) {
+void ALevel::initLvl(const std::string &path)
+{
     back.flushLayers();
-    Parsing::loadCSV(path, [&, this](std::string const &path, int const &i) {
-        if (path.substr(path.find_last_of('.') + 1) == "ogg") {
+    Parsing::loadCSV(path, [&, this](std::string const &path, int const &i)
+    {
+        if (path.substr(path.find_last_of('.') + 1) == "ogg")
+        {
             music.openFromFile(path);
             music.setLoop(true);
-        } else if (path.substr(path.find_last_of('.') + 1) == "png" ||
-                   (path.substr(path.find_last_of('.') + 1) == "jpg"))
+        }
+        else if (path.substr(path.find_last_of('.') + 1) == "png" ||
+                 (path.substr(path.find_last_of('.') + 1) == "jpg"))
             this->back.addLayer(path, i);
     });
 }
 
-void ALevel::drawLvl(sf::RenderWindow &App) {
+void ALevel::drawLvl(sf::RenderWindow &App)
+{
     back.drawBackground(App);
 }
 
-void ALevel::pollEvent(sf::Event &Event) {
-    while (WindowProperties::App->pollEvent(Event)) {
-        switch (Event.type) {
-            case sf::Event::Closed: {
+void ALevel::pollEvent(sf::Event &Event)
+{
+    while (WindowProperties::App->pollEvent(Event))
+    {
+        switch (Event.type)
+        {
+            case sf::Event::Closed:
+            {
                 WindowProperties::gameState = GameState::CLOSE;
                 break;
             }
-            case sf::Event::KeyPressed: {
+            case sf::Event::KeyPressed:
+            {
                 if (Event.key.code == sf::Keyboard::Escape)
                     WindowProperties::gameState = GameState::CLOSE;
                 break;
@@ -216,17 +257,22 @@ void ALevel::mainLoop(GameState currentLevel)
     sf::Event Event;
     sf::Time elapsed;
 
-    while (WindowProperties::App->isOpen() && WindowProperties::gameState == currentLevel) {
+    while (WindowProperties::App->isOpen() && WindowProperties::gameState == currentLevel)
+    {
         elapsed = clock.getElapsedTime();
         invulnerabilityTime = inv.getElapsedTime();
-        if (elapsed.asMilliseconds() > 17) {
+        if (elapsed.asMilliseconds() > 17)
+        {
             clock.restart();
-            if (player->getHp() != 0) {
+            if (player->getHp() != 0)
+            {
                 if (player->getGameMovementMode() == ControlType::KEYBOARD)
                     controller();
                 else if (player->getGameMovementMode() == ControlType::XBOXCONTROLLER)
                     XboxController();
-            } else {
+            }
+            else
+            {
                 this->isGameLost = true;
                 this->soundAttenuationOnDeath -= this->baseSoundAttenuationOnDeathPercentageDecreasing;
                 this->music.setVolume(this->soundAttenuationOnDeath);
