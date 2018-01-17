fe88e82fe78f165f4ab5e42638765e862c23e00e
loiclopez <loic.lopez@epitech.eu>
2018-01-17 17:28:19 +0100

    fix

--- include/Static/WindowProperties.h
+++ include/Static/WindowProperties.h
@@ -28,7 +28,7 @@ public:
         WIN_HEIGHT = videoMode.height;
         WIN_WIDTH = videoMode.width;
         orientation = Orientation::HORIZONTAL;
-        gameState = GameState::LEVEL1;
+        gameState = GameState::LEVEL2;
         App = new sf::RenderWindow(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "R-TYPE", sf::Style::Fullscreen);
         App->setVerticalSyncEnabled(true);
     }
