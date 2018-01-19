35964b9320bc50397754ef061de1c0fed8831d22
loiclopez <loic.lopez@epitech.eu>
2018-01-19 13:33:06 +0100

    remove pushback

--- src/Level/Space.cpp
+++ src/Level/Space.cpp
@@ -4,34 +4,35 @@
 
 #include <Parsing.h>
 #include <Enemy.h>
-#include <cmath>
 #include <Static/PreGame.h>
 #include "Level/Space.h"
 
-Space::Space() : ALevel() {
+Space::Space() : ALevel()
+{
     initLvl("lvl1");
     currentGameLevel = GameState::LEVEL1;
 
-    this->phases.emplace_back([this]() { generateEnemies(EnemyType::BASIC_A, 4, EnemyType::NONE, 0); });
-    this->phases.emplace_back([this]() { generateEnemies(EnemyType::BASIC_A, 6, EnemyType::NONE, 0); });
-    this->phases.emplace_back([this]() { generateEnemies(EnemyType::BASIC_A, 4, EnemyType::BASIC_B, 2); });
-    this->phases.emplace_back([this]() {  generateEnemies(EnemyType::BOSS_A, 1, EnemyType::NONE, 0); });
-    this->phases.emplace_back([this]() {  if (ennemies.empty()) WindowProperties::gameState = GameState::LEVEL2; });
+    this->phases.emplace_back([this]()
+                              { generateEnemies(EnemyType::BASIC_A, 4, EnemyType::NONE, 0); });
+    this->phases.emplace_back([this]()
+                              { generateEnemies(EnemyType::BASIC_A, 6, EnemyType::NONE, 0); });
+    this->phases.emplace_back([this]()
+                              { generateEnemies(EnemyType::BASIC_A, 4, EnemyType::BASIC_B, 2); });
+    this->phases.emplace_back([this]()
+                              { generateEnemies(EnemyType::BOSS_A, 1, EnemyType::NONE, 0); });
+    this->phases.emplace_back([this]()
+                              { if (ennemies.empty()) WindowProperties::gameState = GameState::LEVEL2; });
     phaseMax = static_cast<short>(this->phases.size() - 1);
 }
 
-Space::~Space() {
+Space::~Space()
+{
     music.stop();
     back.flushLayers();
 }
 
-void Space::start() {
-    for (int i = 0; i < (std::rand() % 5 + 5); ++i)
-        ennemies.push_back(std::shared_ptr<IEntity>(new Enemy(EnemyType::BASIC_A)));
-    for (int i = 0; i < (std::rand() % 4 + 1); ++i)
-        ennemies.push_back(std::shared_ptr<IEntity>(new Enemy(EnemyType::BASIC_B)));
-    bulletsEnemy.reserve(100000);
-
+void Space::start()
+{
     PreGame::startPreGameLevel1();
 
     music.play();
